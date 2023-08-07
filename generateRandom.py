db73bb68402d618e2f94b0f24ea0f8051ac24812
FinnGlobal <finn.he0102@gmail.com>
2023-08-07 21:21:08 +1200

    tidy up

--- generateRandom.py
+++
@@ -1,9 +0,0 @@
-# import random
-# listrandom = list(random.Random.randint)
-
-
-
-# list0To107 = []
-
-for i in range(108):
-    print('Value(' + str(i) + ')=' + '"' + str(i) + '"')
--- pysocketUSE.py
+++
@@ -1,6 +0,0 @@
-import pySocket
-
-
-result = pySocket.server.createSocket("54797")
-
-print(result)
